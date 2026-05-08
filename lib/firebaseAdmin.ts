c0b26271468a6006d3b5319821c73bd3c2a915c6
duthinh24896-cloud <ducthinh24896@gmail.com>
2026-05-08 22:30:56 +0700

    fix firebase

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,21 +1,24 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_ADMIN_KEY as string
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(
+  /\\n/g,
+  "\n"
 );
 
-export const adminApp =
-  admin.apps.length
-    ? admin.app()
-    : admin.initializeApp({
-        credential:
-          admin.credential.cert(
-            serviceAccount
-          ),
-      });
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId:
+        process.env.FIREBASE_PROJECT_ID,
 
-export const adminAuth =
-  admin.auth();
+      clientEmail:
+        process.env.FIREBASE_CLIENT_EMAIL,
 
-export const adminDb =
-  admin.firestore();
+      privateKey,
+    }),
+  });
+}
+
+export const adminAuth = admin.auth();
+
+export const adminDb = admin.firestore();
